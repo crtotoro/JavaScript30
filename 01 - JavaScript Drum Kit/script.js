df2b569bb4fcb441a69349fbfb34241deee09ae7
leaky3 <crtotoro@gmail.com>
2023-11-12 14:27:37 -0600

    Complete Challenge 1

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,14 @@
+window.addEventListener('keydown', (e) => {
+  const audio = document.querySelector(`audio[data-key=${e.code}]`);
+  const key = document.querySelector(`.key[data-key=${e.code}]`);
+  if(!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+});
+
+window.addEventListener('keyup', (e) => {
+  const key = document.querySelector(`.key[data-key=${e.code}]`);
+  if(!key) return;
+  key.classList.remove('playing');  
+});
